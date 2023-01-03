199ac336d3951be8fb5245c316997d1361d10845
murat-boz <muratboz@hotmail.com>
2023-01-03 23:36:06 +0300

    Add project files.

---
+++ ReactiveExtension.HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace ReactiveExtension.HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Observable
+                .Range(1, 10)
+                .Select(x => 10 / (5 - x));
+
+            list.Subscribe(
+                x  => Console.WriteLine($"Received {x}"),
+                ex => Console.WriteLine($"An error has occurred on value {ex}"),
+                () => Console.WriteLine("Done"));
+
+            Console.ReadLine();
+        }
+    }
+}
